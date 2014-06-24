e8d9aaece4dbdd5115e98cbb9f3be4574bb521a0
Friedrich Hemer <friedrich.hemer@gmail.com>
2014-06-24 12:05:15 +0200

    readme test

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 LipaugusIdentification
 ======================
 
+
+<a href="http://d3js.org"><img src="http://d3js.org/logo.svg" align="left" hspace="10" vspace="6"></a>
+
+
+
+
 Detect and identify individuals of a lek
+
+<a href="http://d3js.org"><img src="http://d3js.org/logo.svg" align="left" hspace="15" vspace="3"></a>
--- lipoalgo.m
+++ lipoalgo.m
@@ -7,6 +7,7 @@ lipoalgopaths;
 global fs nfft ovlp T F
 
 % Read .wav file
+%[x, fs] = audioread('lipo.WAV');
 [x, fs] = audioread('120119_071_mono3.WAV',[round(2400*44100) round(2600*44100)]);
 
 % Stereo to mono
@@ -111,7 +112,7 @@ end
 hold off
 toc
 %%%%%%%%%%%%%%%%%%%%%%%
-%% Treatment 
+% Treatment 
 %%%%%%%%%%%%%%%%%%%%%%%
 
 for i = 2:length(maxtab(:,1))
@@ -132,54 +133,13 @@ for i = 2:length(maxtab(:,1))
     BWs = [BWsa, BWsaf];
     m = rmnoisepts(BWs,35); %%%%%%%%%%% /!\ ARBITRARY CONST %%%%%%%%%%%
     
-    % Transform m to 3 vectors x,y (positions) and w the weighted vector
-    %[x, y, w] = mat3vec(m);
-
+   
     figure(1)
     xlim([co2time(tinf)-3 co2time(tsup)+3])
     
-    % Plot
-    %figure(2)
-    %subplot(131), plotmat(T(trange),F(frange),log(Sreca)); title('Raw')
-    %subplot(132), plotmat(m); title('The matrix to treat'), hold on % T(trange),F(frange),
-    
-    
-    
-    
-%     [x, y, w, h] = checkcorec(J-6,I-10,12,20,m);
-%     rectangle('Position',[x, y, w, h],...
-%             'LineWidth',2,...
-%             'LineStyle','--',...
-%             'EdgeColor','r')
-        
-    %m(m<0.15*max(m(:))) = 0;    
     
     mod = zeros(size(m));
     mask = ones(size(m));
-    
-    %subplot(133), plotmat(m); title('15% less'), hold on
-    
-    
-    
-%     srec = m(y:y+h,x:x+w);
-%     [x, y, w] = mat3vec(srec);
-%     
-%     figure(3), plotmat(srec)
-    %edg = findedges(srec);
-    %BW = mergeBWs(edg,4);
-    %disp(BW)
-    %figure(4), plotmat(BW)
-
-%     mr = m(:,:).^3;
-%     %mr(mr<0.4*max(mr(:))) = 0;
-%     [x, y, w] = mat3vec(mr);
-%     figure(3), plotmat(mr), hold on,
-%     [fitresult, gof] = createFitSmooth(x,y,w);
-%     plot(fitresult), hold off
-    
-
-
-
 
 
     %%%%%%%%%%%%%%%%%%%%%%%
@@ -194,7 +154,6 @@ for i = 2:length(maxtab(:,1))
     figure(2)
     subplot(131), plotmat(T(trange),F(frange),log(Sreca)); title('Raw')
     subplot(132), plotmat(m); title('The matrix to treat')
-    %subplot(133), plotmat(m.*mask); title('With mask')
 
     pocs = {};
 
@@ -205,13 +164,12 @@ for i = 2:length(maxtab(:,1))
         %I = co2freq(I+freq2co(LOWR));
         %J = co2time(J)+co2time(tinf);
 
-        maxh = 3;
+        maxh = 5;
         maxv = 3;
 
         poc = []; % piece of curve
-        for vdir = -1:2:1 % direction vertical (monte ou descend)
+        for vdir = -1:2:1 % direction vertical (+1 monte, -1 descend)
             % Initialisation x, y
-            %disp('Init')
             x = J;
             y = I;
 
@@ -227,21 +185,21 @@ for i = 2:length(maxtab(:,1))
                 if isequal(mnew(y,x),0)
                     [xout, yout, bool] = lookleft(x, y, mnew);
                     if bool
-                        %disp('Funky left')
+                        % Funky left
                         x = xout;
                         y = yout;
                         ENDH = 1;
                     else
-                        %disp('Going right')
-                        hdir = 1; % direction horizontale (gauche ou droite)
+                        % Going right
+                        hdir = 1;
                     end
                 else
-                    %disp('Going left')
+                    % Going left
                     hdir = -1;
                 end
 
                 while ~ENDH && hdec < maxh
-                    %disp('Searching for junction …')
+                    % Searching for junction …
                     % tant qu'on n'a pas rencontré une jonction (0, ~0), on se décale (5x max) 
                     hdec = hdec + 1; % on incrémente le décalage
                     [x, y, ENDH] = checkandgo(x, y, mnew, hdir); % on tente le décalalage
@@ -250,14 +208,12 @@ for i = 2:length(maxtab(:,1))
                 end
 
                 if isequal(ENDH,1)
-                    %disp('Found a junction')
+                    % Found a junction
                     % on poursuit
                     poc = [poc; x y]; % append (x, y) to the list 
                     [x2, ~] = checkco(x + 15, y, mnew);
                     mask(y, x:x2) = 0; % update mask
-                    %size(mask)
                     %figure(2), subplot(133), plotmat(mnew), hold on, plot(x,y,'*g'), hold off 
-                    %waitforbuttonpress;
                     vdec = 0;
                 else
                     x = x - hdir*hdec;
@@ -286,16 +242,8 @@ for i = 2:length(maxtab(:,1))
     hold off
 
 
-
-
-
-
-
-
-
-
-%     a = 1;
-%     while a
-%         a = ~waitforbuttonpress;
-%     end
+    a = 1;
+    while a
+        a = ~waitforbuttonpress;
+    end
 end
