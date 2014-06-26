1c2e604d1f42a4bccb66f804c822d6244ea5f705
Friedrich Hemer <friedrich.hemer@gmail.com>
2014-06-26 11:38:28 +0200

    add doc

--- measurmentsfn/lookleft.m
+++ measurmentsfn/lookleft.m
@@ -1,10 +1,17 @@
 function [xnew, ynew, bool] = lookleft(x, y, m)
+% Called in case of the following situation to find B px when you are in A.
+%      ___
+%     |i,j|
+%     |_B_| ___
+%          |x,y|
+%          |_A_|
+%   
     xnew = x;
     ynew = y;
     zerotoone = 0;
     onetozero = 0;
     dec = 0;
-    while (~zerotoone || ~onetozero) && dec <= 3
+    while (~zerotoone || ~onetozero) && dec <= 3  %%%%%%%%%%% /!\ ARBITRARY CONST %%%%%%%%%%%
         [xnew, ynew] = checkco(xnew - 1, ynew, m);
         if ~onetozero && ~zerotoone && ~isequal(m(ynew,xnew),0)
             zerotoone = 1;
