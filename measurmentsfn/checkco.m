9469fc018d216e9db61ea74907687ed91ff030e3
Friedrich Hemer <friedrich.hemer@gmail.com>
2014-06-26 16:47:46 +0200

    adapt for vector input

--- measurmentsfn/checkco.m
+++ measurmentsfn/checkco.m
@@ -3,16 +3,18 @@ function [x, y] = checkco(x, y, m)
 % adapt x, y, to define corrects coordinates of m.
 % -------------------------------------------------------------------------
     [mx, my] = size(m);
-    if x < 1
-        x = 1;
-    end
-    if y < 1
-        y = 1;
-    end
-    if x > my
-        x = my;
-    end
-    if y > mx
-        y = mx;
+    for i = 1:length(x)
+        if x(i) < 1
+            x(i) = 1;
+        end
+        if y(i) < 1
+            y(i) = 1;
+        end
+        if x(i) > my
+            x(i) = my;
+        end
+        if y(i) > mx
+            y(i) = mx;
+        end
     end
 end
