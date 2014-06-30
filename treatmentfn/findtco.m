6811553f6ccbd5073a933557dc257afdbc96f8c5
Friedrich Hemer <friedrich.hemer@gmail.com>
2014-06-30 10:25:10 +0200

    comment

--- treatmentfn/findtco.m
+++ treatmentfn/findtco.m
@@ -1,10 +1,13 @@
-function [tinf, tsup] = findtco(val, shftw)
+function [tinf, tsup] = findtco(val, shftw, my)
 % Given a coordinate (val) and a size of shifting windows (shftw), it
 % returns begin and end coordinates.
 %--------------------------------------------------------------------------
-    tinf = val-time2co(0.15);
+    tinf = val-time2co(0.15); % slight shift to the left
     tsup = val+shftw;
     if tinf < 1
         tinf = 1;
+    end
+    if tsup > my
+        tsup = my;
     end
 end
