2ce6b904039f500b8a282bb11f428f2b904f2f94
Friedrich Hemer <friedrich.hemer@gmail.com>
2014-07-03 12:40:50 +0200

    fix bug

--- conversion/time2co.m
+++ conversion/time2co.m
@@ -8,6 +8,9 @@ function out = time2co(varargin)
     out = zeros(1,length(varargin));
     for i = 1:length(varargin)
         ind = find(T<varargin{i});
+        if isempty(ind)
+            ind = 1;
+        end
         out(i) = ind(end);
     end
 end
