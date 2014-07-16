7321c71c51b868216ae4818a67c7d6de20437a86
Friedrich Hemer <friedrich.hemer@gmail.com>
2014-07-16 17:50:12 +0200

    simplify treatment (template instead of lines

--- treatmentfn/mergeBWs.m
+++ treatmentfn/mergeBWs.m
@@ -1,20 +1,20 @@
-function BW = mergeBWs(varargin)
+function BW = mergeBWs(BWs)
 %
 %--------------------------------------------------------------------------
     BW = 0;
-    if isequal(size(varargin,2),3)
-        BWs = varargin{1};
-        minpixgrp = varargin{2};
-        mask = varargin{3};
-    elseif isequal(size(varargin,2),2)
-        BWs = varargin{1};
-        minpixgrp = varargin{2};
-        mask = ones(size(BWs{1}));
-    end
+%     if isequal(size(varargin,2),3)
+%         BWs = varargin{1};
+%         minpixgrp = varargin{2};
+%         mask = varargin{3};
+%     elseif isequal(size(varargin,2),2)
+%         BWs = varargin{1};
+%         minpixgrp = varargin{2};
+%         mask = ones(size(BWs{1}));
+%     end
     
     for i = 1:length(BWs)
-        BWs{i} = BWs{i}.*mask;
-        BWs{i} = bwareaopen(BWs{i}, minpixgrp); %%%%%%%%%%% /!\ ARBITRARY CONST %%%%%%%%%%%
+%         BWs{i} = BWs{i}.*mask;
+%         BWs{i} = bwareaopen(BWs{i}, minpixgrp); %%%%%%%%%%% /!\ ARBITRARY CONST %%%%%%%%%%%
         BW = BW + double(BWs{i});
     end
 
